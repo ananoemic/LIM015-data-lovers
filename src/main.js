1b12055776171b6671d044c0dec57f9c4a2b6e48
KeylaMia <keylamialujimenezg@gmail.com>
2021-07-13 10:44:57 -0500

    agregando diseño responsive en html y css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-import { example } from './data/ghibli.js';
+import data from './data/ghibli/ghibli.js';
 
+ 
+function peliculas (){
 
+const filmshtml = document.getElementById("films"); 
+const filmsdata = data["films"];
 
-console.log(example, data);
+ let htmls="";
+
+ filmsdata.forEach(pelicula =>{
+
+    let auxPelicula = `<div class="films"> 
+        <img src= "${pelicula['poster']}">
+            <div>
+            //h3= etiqueta de html cabecera que anida subsecciones
+            <h3>"${pelicula['title']}"</h3> 
+            <p> 
+            "${pelicula['description']}"
+            </p>
+            </div>
+    </div>`;
+
+    htmls+=auxPelicula;
+
+ }),
+
+ filmshtml.innerHTML=htmls
+
+}
+
+ peliculas ()
 
