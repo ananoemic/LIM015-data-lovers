12c6daf30a9c6c514645d76d2e2defe3ea68f866
ananoemic <83612889+ananoemic@users.noreply.github.com>
2021-07-13 20:35:53 -0500

    merge error

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data/ghibli.js';
+import data from './data/ghibli/ghibli.js';
 
+ 
+function peliculas (){
 
+const filmshtml = document.getElementById("films"); 
+const filmsdata = data["films"];
 
-console.log(example, data);
+ let htmls="";
+
+ filmsdata.forEach(pelicula =>{
+
+    let auxPelicula = `<div class="films"> 
+        <img src= "${pelicula['poster']}">
+            <div>
+    
+            <h3>"${pelicula['title']}"</h3> 
+            <p> 
+            "${pelicula['description']}"
+            </p>
+            <p> Director: "${pelicula['director']}"
+            </p>
+            <p> Producer: "${pelicula['producer']}"
+            </p>
+            
+            </div>
+    </div>`;
+
+    htmls+=auxPelicula;
+
+ }),
+
+ filmshtml.innerHTML=htmls
+
+}
+
+ peliculas ()
 
