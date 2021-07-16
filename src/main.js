01606335c8098bcf9c968c499266cf528e24f28a
Emily3000 <emily.ferpanebra@gmail.com>
2021-07-15 21:37:06 -0500

    previa del js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,36 @@
-import data from 'data/ghibli/ghibli.js';
-
-const btnFilms= document.getElementById("btnFilms");
-const btnAbout=document.getElementById("btnAboutUs");
-const slider= document.getElementById("slider");
-const filmsContainer= document.getElementById("filmsContainer");
-const aboutContainer= document.getElementById("aboutUsContainer");
-
-//funcion mostrar ocultar peliculas 
-
-function showFilms(){
-    slider.style.display="none";
-    filmsContainer.style.display="block";
-    }
-
-btnFilms.addEventListener('click',showFilms);
+import data from './data/ghibli/ghibli.js';
 
 
+
+
+
+
+function peliculas (){
+
+const filmshtml = document.getElementById("films"); 
+const filmsdata = data["films"];
+
+ let htmls="";
+
+ filmsdata.forEach(pelicula =>{
+
+    let auxPelicula = `<div class="films"> 
+        <img src= "${pelicula['poster']}">
+            <div>
+            //h3= etiqueta de html cabecera que anida subsecciones
+            <h3>"${pelicula['title']}"</h3> 
+            <p> 
+            "${pelicula['description']}"
+            </p>
+            </div>
+    </div>`;
+
+    htmls+=auxPelicula;
+
+ }),
+
+ filmshtml.innerHTML=htmls
+
+}
+
+ peliculas ()
