5cdb31b137d3bd7cbce8e81bd940d3217616c5ac
Emily3000 <emily.ferpanebra@gmail.com>
2021-07-15 22:44:36 -0500

    cambios realizados en films.html y en el main.js -- no cambiar los ids creados

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import data from './data/ghibli/ghibli.js';
-
-
-
-
-
-
-function peliculas (){
-
-const filmshtml = document.getElementById("films"); 
 const filmsdata = data["films"];
+const filmshtml = document.getElementById("peliculasData"); 
 
  let htmls="";
 
+ function peliculas(){
  filmsdata.forEach(pelicula =>{
 
     let auxPelicula = `<div class="films"> 
         <img src= "${pelicula['poster']}">
             <div>
-            //h3= etiqueta de html cabecera que anida subsecciones
             <h3>"${pelicula['title']}"</h3> 
             <p> 
             "${pelicula['description']}"
@@ -27,10 +19,38 @@ const filmsdata = data["films"];
 
     htmls+=auxPelicula;
 
- }),
+ });
 
  filmshtml.innerHTML=htmls
-
 }
 
- peliculas ()
+peliculas ()
+
+
+
+
+ function filterData(data,conditions){
+  
+ }
+
+ function orderData(data,sortOrder){
+
+ }
+
+let botonFiltrar=document.getElementById('filtrar');
+botonFiltrar.addEventListener('click',()=>{
+    let filterBy="";
+    filterData(filmsdata,filterBy);
+})
+
+let botonOrdenar=document.getElementById('ordenar');
+botonOrdenar.addEventListener('click',()=>{
+    
+    let orderBy=document.getElementById('selectOrden').value;
+    console.log(orderBy);
+    orderData(filmsdata,orderBy)
+
+});
+
+
+
