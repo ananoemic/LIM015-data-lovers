3c49b0a4a11be0e8310497b2e414b5dff9b2a79b
KeylaMia <keylamialujimenezg@gmail.com>
2021-07-18 22:58:09 -0500

    "cambios Emy"

--- src/main.js
+++ src/main.js
@@ -1,42 +1,13 @@
 import data from './data/ghibli/ghibli.js';
-
-const btnFilms= document.getElementById("btnFilms");
-const btnAbout=document.getElementById("btnAboutUs");
-const logoHome= document.getElementById("logoHome");
-const logoHome2=document.getElementById("logoHome2")
-const slider= document.getElementById("slider");
-const filmsContainer= document.getElementById("filmsContainer");
-const aboutContainer= document.getElementById("aboutUsContainer");
+​
 const filmsdata = data["films"];
 const filmshtml = document.getElementById("peliculasData"); 
-
-//funcion mostrar ocultar aboutUs 
-
-btnAbout.addEventListener('click', ()=>{
-    slider.style.display="none";
-    filmsContainer.style.display="none";
-    aboutContainer.style.display="block";
-})
-
-//funcion mostrar ocultar home
-
-logoHome.addEventListener('click', ()=>{
-    filmsContainer.style.display="none";
-    aboutContainer.style.display="none";
-    slider.style.display="block";
-})
-
-btnFilms.addEventListener('click', ()=>{
-    filmsContainer.style.display="block";
-    aboutContainer.style.display="none";
-    slider.style.display="none";
-})
-
-
-let htmls="";
-function peliculasDefault(){
+​
+ let htmls="";
+​
+ function peliculasDefault(){
  filmsdata.forEach(pelicula =>{
-
+​
     let auxPelicula = `<div class="films"> 
         <img src= "${pelicula['poster']}">
             <div>
@@ -58,17 +29,18 @@ function peliculasDefault(){
             </p>
             </div>
     </div>`;
-
+​
     htmls+=auxPelicula;
-
+​
  });
-
- filmshtml.innerHTML=htmls
+​
+ filmshtml.innerHTML=htmls;
 }
-
-peliculasDefault();// Ejecutamos por defecto la función
-
-
+​
+peliculasDefault();
+// Ejecutamos por defecto la función; cuando carga las peliculas al inicio (carga pagina films)
+​
+​
 function peliculasPorSeleccion(peliculas){
     htmls="";
     peliculas.forEach(pelicula =>{
@@ -99,16 +71,16 @@ function peliculasPorSeleccion(peliculas){
      });
      filmshtml.innerHTML=htmls;
 }
-
-
+​
+​
  function filterData(data,conditions){
   //Data son las peliculas
   //El resulta ya tiene las peliculas filtradas
     let resultFilter=data.filter(pelicula=>pelicula['director']==conditions)
     return resultFilter;
-
+​
  }
-
+​
  function orderData(data,conditions){
     let resultOrder=[];
     // Lo que 1ero que debemos saber es la condicion,
@@ -116,9 +88,9 @@ function peliculasPorSeleccion(peliculas){
     
             if(conditions==1){  
             // Esta condicion es para ordenar A-Z 
-
+​
             resultOrder=data.sort(function (p1, p2){
-
+​
                     if(p1["title"]>p2["title"]){
                     return 1;
                     }
@@ -127,11 +99,11 @@ function peliculasPorSeleccion(peliculas){
                     }
                     return 0;
                 })
-
+​
               }else if(conditions==2){ 
             // Esta condicion es para ordenar Z-A
                  resultOrder=data.sort(function (p1, p2){
-
+​
                     if(p1["title"]<p2["title"]){
                     return 1;
                     }
@@ -140,12 +112,12 @@ function peliculasPorSeleccion(peliculas){
                     }
                     return 0;
                 })
-
+​
               }else if(conditions==3){
             // Esta condicion es para ordenar Year(+)
             
             resultOrder=data.sort(function (p1, p2){
-
+​
                 if(p1["release_date"]>p2["release_date"]){
                 return 1;
                 }
@@ -154,11 +126,11 @@ function peliculasPorSeleccion(peliculas){
                 }
                 return 0;
             })
-
+​
               }else if(conditions==4){
             //Esta condicion es para ordenar Year(-)  
             resultOrder=data.sort(function (p1, p2){
-
+​
                 if(p1["release_date"]<p2["release_date"]){
                 return 1;
                 }
@@ -171,7 +143,7 @@ function peliculasPorSeleccion(peliculas){
               }else if(conditions==5){
             //Esta condicion es para ordenar Score(+)
             resultOrder=data.sort(function (p1, p2){
-
+​
                 if(p1["rt_score"]>p2["rt_score"]){
                 return 1;
                 }
@@ -179,15 +151,15 @@ function peliculasPorSeleccion(peliculas){
                 return -1;
                 }
                 return 0;
-
+​
             })
-
-
+​
+​
               }else if(conditions==6){
                 //Esta condicion es para ordenar Score(-)
-
+​
                 resultOrder=data.sort(function (p1, p2){
-
+​
                     if(p1["rt_score"]<p2["rt_score"]){
                     return 1;
                     }
@@ -198,13 +170,13 @@ function peliculasPorSeleccion(peliculas){
     
                 })
             
-
+​
               }
               return resultOrder;
-
-
+​
+​
  }
-
+​
 let botonFiltrar=document.getElementById('filtrar');
 botonFiltrar.addEventListener('click',(e)=>{
     e.preventDefault();
@@ -213,7 +185,7 @@ botonFiltrar.addEventListener('click',(e)=>{
     
     peliculasPorSeleccion(result);
 })
-
+​
 let botonOrdenar=document.getElementById('ordenar');
 botonOrdenar.addEventListener('click',()=>{
     
@@ -221,5 +193,7 @@ botonOrdenar.addEventListener('click',()=>{
     //console.log(orderBy);
     let result=orderData(filmsdata,orderBy);
    peliculasPorSeleccion(result);
-
+​
 });
+​
+​
