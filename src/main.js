bd22965385e4d0b052c396fc5b20c1536a330a6b
KeylaMia <keylamialujimenezg@gmail.com>
2021-07-18 19:51:57 -0500

    modificaciones html y js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import data from 'data/ghibli/ghibli.js';
+import data from './data/ghibli/ghibli.js';
 
 const btnFilms= document.getElementById("btnFilms");
 const btnAbout=document.getElementById("btnAboutUs");
+const logoHome= document.getElementById("logoHome");
 const slider= document.getElementById("slider");
 const filmsContainer= document.getElementById("filmsContainer");
 const aboutContainer= document.getElementById("aboutUsContainer");
@@ -9,10 +10,26 @@ const aboutContainer= document.getElementById("aboutUsContainer");
 //funcion mostrar ocultar peliculas 
 
 function showFilms(){
-    slider.style.display="none";
     filmsContainer.style.display="block";
+    slider.style.display="none";
+    aboutContainer.style.display="none";
     }
 
 btnFilms.addEventListener('click',showFilms);
 
+//funcion mostrar ocultar aboutUs 
+
+btnAbout.addEventListener('click', ()=>{
+    slider.style.display="none";
+    filmsContainer.style.display="none";
+    aboutContainer.style.display="block";
+})
+
+//funcion mostrar ocultar home
+
+logoHome.addEventListener('click', ()=>{
+    filmsContainer.style.display="none";
+    aboutContainer.style.display="none";
+    slider.style.display="block";
+})
 
