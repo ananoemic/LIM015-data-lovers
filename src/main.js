a69b63a4d564b75a6803627543a74398a55d495f
KeylaMia <keylamialujimenezg@gmail.com>
2021-07-22 16:59:20 -0500

    cambios keyla no cuentan

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import data from "./data/ghibli/ghibli.js";
 
 const filmsdata = data["films"];
+console.log(filmsdata);
 const filmshtml = document.getElementById("peliculasData");
 
 let htmls = "";
